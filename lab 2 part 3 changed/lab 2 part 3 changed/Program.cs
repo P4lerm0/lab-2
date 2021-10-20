3dfae6a5ba876a531c9ddfe424570b754a2fdc46
P4lerm0 <90239535+P4lerm0@users.noreply.github.com>
2021-10-20 10:01:44 +0300

    Add files via upload

---
+++ lab 2 part 3 changed/lab 2 part 3 changed/Program.cs
@@ -0,0 +1,7 @@
+﻿double x, y, z, f;
+x = double.Parse(Console.ReadLine());
+y = double.Parse(Console.ReadLine());
+z = double.Parse(Console.ReadLine());
+f = (1 / 19.0) + Math.Log(Math.Abs(y) / 2) + z / 3 + Math.Max(x / 4, 2);
+Console.WriteLine(f);
+Console.ReadKey();
